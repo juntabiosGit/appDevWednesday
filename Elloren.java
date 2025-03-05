4d2bbdb9c82abae8613a986f8b263e78b2e45816
johnpeter <jelloren@paterostechnologicalcollege.com>
2025-03-05 18:07:43 +0800

    Added Elloren.java with age calculation and updated week5.java

---
+++ Elloren.java
@@ -0,0 +1,8 @@
+public class Elloren {
+    public static void display() {
+        int age = 25; // Replace with user input if necessary
+        System.out.println("Hello! This is Elloren contributing to the project.");
+        System.out.println("My current age is: " + age);
+        System.out.println("Next year, I will be: " + (age + 1));
+    }
+}
