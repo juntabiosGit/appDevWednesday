9507d3c73754625402dec4c3b335e0e0860dafff
fcandes <fcandes@paterostechnologicalcollege.edu.ph>
2025-03-05 18:29:11 +0800

    Add files via upload

--- week5.java
+++ week5.java
@@ -3,23 +3,6 @@ public class week5 {
         System.out.println("Collaborative Java Project\n");
 
         // Call each student's method
-
-        //Olimberio.display(); // Add your method call here
-
-
-        Gardoce.display(); // Add your method call here
-
-        
-
-        Elloren.display();
-
-
-	     Cardenas.display();
-
-        Orolfo.display();
-
-
-
-
+        //lastname.display(); // Add your method call here
     }
 }
