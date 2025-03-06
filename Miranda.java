ae332d47fdf08274181f1ff6330c2270913ca9f5
elibert141 <emiranda@paterostechnologicalcollege.edu.ph>
2025-03-06 14:25:05 +0800

    Added Miranda.java with age calculation and updated week5.java

--- Miranda.java
+++ Miranda.java
@@ -1,6 +1,6 @@
-public class Cruz {
+public class Miranda {
     public static void display() {
-        int age = 20; // Replace with user input if necessary
+        int age = 21; // Replace with user input if necessary
         System.out.println("Hello! This is Cruz contributing to the project.");
         System.out.println("My current age is: " + age);
         System.out.println("Next year, I will be: " + (age + 1));
