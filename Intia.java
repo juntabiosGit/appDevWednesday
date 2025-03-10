47297860d7f46f52bd83a7a2dcf2ba6b164220e7
Intiaa <bpintia@paterostechnologicalcollege.edu.ph>
2025-03-10 14:14:23 +0800

    Update Intia.java

--- Intia.java
+++ Intia.java
@@ -1,7 +1,7 @@
-public class Gupid {
+public class Intia {
     public static void display() {
-        int age = 24; // Replace with user input if necessary
-        System.out.println("Hello! This is Gupid contributing to the project.");
+        int age = 21; // Replace with user input if necessary
+        System.out.println("Hello! This is Intia contributing to the project.");
         System.out.println("My current age is: " + age);
         System.out.println("Next year, I will be: " + (age + 1));
     }
