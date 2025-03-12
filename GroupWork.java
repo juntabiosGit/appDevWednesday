dbbd46274fee279529d3aa62875dc964baaf114e
gerald <ghmirandilla@paterostechnologicalcollege.edu.ph>
2025-03-12 21:15:56 +0800

    Group 5: Added modulos method(Mirandilla,)

--- GroupWork.java
+++ GroupWork.java
@@ -9,10 +9,9 @@ public class GroupWork   {
         System.out.print("Enter second number: ");
         int num2 = scanner.nextInt();
        
-        // Calling methods from Calculator.java
-        System.out.println("Sum: " + Calculator.add(num1, num2));  // Group 1's method
+     
+        System.out.println("Remainder: " + Calculator.mod(num1, num2));  // Group 5's method
         
-
-       System.out.println("Are numbers equal? " + Calculator.isEqual(num1, num2));  // Group 10's method
         scanner.close();
     }
+}
