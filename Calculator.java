dce408345c4d5d9d0d39b0790dbde4dd35d5096d
johnpeter <jelloren@paterostechnologicalcollege.com>
2025-03-12 20:48:48 +0800

    Group 1: Added addition method ( john )

--- Calculator.java
+++ Calculator.java
@@ -1,14 +1,10 @@
 // Calculator.java
 public class Calculator {
-   
-    // Group 1: Addition (Members: John, Sarah, Alex)
-    public static int add(int a, int b) {
-        return a + b;
-    }
 
-    // Group 10: Check if numbers are equal (Members: Jake, Emily, Ryan)
-    public static boolean isEqual(int a, int b) {
-        return a == b;
+
+    // Group 3: multiply(Members: Elloren)
+    public static int multiple(int a, int b) {
+        return a * b;
     }
 
     // Other groups will add their methods here
