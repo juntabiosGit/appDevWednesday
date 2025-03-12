eb531245c4d62ac1551d1a1327358c4563e06b05
larajeangupid <lgupid@paterostechnologicalcollege.edu.ph>
2025-03-12 21:32:34 +0800

    Group 5: Added Modulos Method(Mirandilla,Gardoce,Gupid)

--- Calculator.java
+++ Calculator.java
@@ -1,15 +1,13 @@
 // Calculator.java
 public class Calculator {
    
-    // Group 1: Addition (Members: John, Sarah, Alex)
-    public static int add(int a, int b) {
-        return a + b;
+    // Calculator.java
+public class Calculator {
+   
+    // Group 5: Modulus (Members:Mirandilla,Gardoce,Gupid)
+    public static int mod(int a, int b) {
+        return a % b;
     }
 
-    // Group 10: Check if numbers are equal (Members: Jake, Emily, Ryan)
-    public static boolean isEqual(int a, int b) {
-        return a == b;
-    }
-
-    // Other groups will add their methods here
 }
+
