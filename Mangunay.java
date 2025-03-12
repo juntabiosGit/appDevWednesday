348850e9328a8bf7a1afb31e9a0ab54d88b67b5c
sirjun <juntabios75@gmail.com>
2025-03-12 19:51:55 +0800

    dele

--- Mangunay.java
+++
@@ -1,9 +0,0 @@
-public class Mangunay {
-    public static void display() {
-        int age = 19;
-        System.out.println("Hello! This is Mangunay contributing to the project.");
-        System.out.println("My current age is: " + age);
-        System.out.println("Next year, I will be: " + (age + 1));
-    }
-}
-
