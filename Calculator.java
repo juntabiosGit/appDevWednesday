0c634f51a8c11d1a2c878ad4ce8e713dcd74ad2f
RoyOngue <rongue@paterostechnologicalcollege.edu.ph>
2025-03-12 21:49:13 +0800

    Group 5: Added Modulos Method(Mirandilla,Gardoce,Gupid,Ongue)

--- Calculator.java
+++ Calculator.java
@@ -1,15 +1,9 @@
 // Calculator.java
 public class Calculator {
    
-    // Group 1: Addition (Members: John, Sarah, Alex)
-    public static int add(int a, int b) {
-        return a + b;
+    // Group 5: Modulus (Members: Mirandilla,Gardoce,Gupid,Ongue)
+    public static int mod(int a, int b) {
+        return a % b;
     }
 
-    // Group 10: Check if numbers are equal (Members: Jake, Emily, Ryan)
-    public static boolean isEqual(int a, int b) {
-        return a == b;
-    }
-
-    // Other groups will add their methods here
 }
