30a21560568912b242656ebb588d10f987540781
orolfo <kmorolfo@paterostechnologicalcollege.edu.ph>
2025-03-12 20:42:42 +0800

    Group 6: Added square method (Orolfo, Laygan)

--- Calculator.java
+++ Calculator.java
@@ -6,15 +6,14 @@ public class Calculator {
         return a + b;
     }
 
-// Group 6: Square (Members: Orolfo, Laygan)
-public static int square(int a) {
-    return a * a;
-}
-
     // Group 10: Check if numbers are equal (Members: Jake, Emily, Ryan)
     public static boolean isEqual(int a, int b) {
         return a == b;
     }
 
     // Other groups will add their methods here
+}
+// Group 6: Square (Members: Orolfo, Laygan)
+public static int square(int a) {
+    return a * a;
 }
