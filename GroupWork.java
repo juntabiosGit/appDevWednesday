4adc2e3deac16cbc5c6a59c137352e2395796e52
juntabiosGit <165021843+juntabiosGit@users.noreply.github.com>
2025-03-12 21:09:27 +0800

    UPDATED AS OF 9:09

--- GroupWork.java
+++ GroupWork.java
@@ -14,10 +14,9 @@ public class GroupWork   {
         System.out.println("Sum: " + Calculator.multiple(num1, num2));  // Group 2's method
 
         System.out.println("subtract: " + Calculator.add(num1, num2));  // Group 2's method
-
         
 
-       System.out.println("Are numbers equal? " + Calculator.isEqual(num1, num2));  // Group 10's method
+        //System.out.println("Are numbers equal? " + Calculator.isEqual(num1, num2));  // Group 10's method
         scanner.close();
     }
 
