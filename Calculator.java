247c2dda00ad1d8256f6a96d0ac1edc8f91a3650
juntabiosGit <165021843+juntabiosGit@users.noreply.github.com>
2025-03-12 21:08:40 +0800

    UPDATED AS OF 9:08

--- Calculator.java
+++ Calculator.java
@@ -1,22 +1,21 @@
 // Calculator.java
 public class Calculator {
-
-
-
     // Group 3: multiply(Members: Elloren)
     public static int multiple(int a, int b) {
         return a * b;
-
+    }
    
     // Group 2: Subtraction (Members: Brisulda)
     public static int subtract(int a, int b) {
         return a - b;
-
     }
 
     // Other groups will add their methods here
-}
+
+
 // Group 6: Square (Members: Orolfo, Laygan)
 public static int square(int a) {
     return a * a;
+}
+
 }
