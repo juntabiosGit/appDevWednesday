ef45b9b7c03aeb143a8f4d687a4e67d56c2e0376
James <jmbdelacruz@paterostechnologicalcollege.edu.ph>
2025-03-13 17:55:18 +0800

    Group 2: Added the subtraction method (Brisulda, Cardenas, Dela Cruz)

--- Calculator.java
+++ Calculator.java
@@ -5,7 +5,7 @@ public class Calculator {
         return a * b;
     }
    
-    // Group 2: Subtraction (Members: Brisulda)
+    // Group 2: Subtraction (Members: Brisulda, Dela Cruz)
     public static int subtract(int a, int b) {
         return a - b;
     }
