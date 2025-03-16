68796048dec1b482787ce93abcfa72010c33c008
Intia Beverly P. <bpintia@paterostechnologicalcollege.edu.ph>
2025-03-16 11:52:30 +0800

    Group 5:Added Modulos Method(Mirandilla,Gardoce,Gupid,Gigante,Ongue,Capuno,Intia)

--- Calculator.java
+++ Calculator.java
@@ -1,21 +1,9 @@
 // Calculator.java
 public class Calculator {
-    // Group 3: multiply(Members: Elloren)
-    public static int multiple(int a, int b) {
-        return a * b;
-    }
    
-    // Group 2: Subtraction (Members: Brisulda)
-    public static int subtract(int a, int b) {
-        return a - b;
+    // Group 5: Modulus (Members:Mirandilla,Gardoce,Gupid,Gigante,Ongue,Capuno,Intia)
+    public static int mod(int a, int b) {
+        return a % b;
     }
-
-    // Other groups will add their methods here
-
-
-// Group 6: Square (Members: Orolfo, Laygan)
-public static int square(int a) {
-    return a * a;
-}
 
 }
