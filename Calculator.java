7d644a7f405a711544335d92e8f9328ebdb978d7
afernandezgit <your-email@example.com>
2025-03-18 01:45:47 +0800

    Group 5: Added Modulos Method(Mirandilla,Gardoce,Gupid,Ongue,Gigante,Capuno,Intia,Fernandez)

---
+++ Calculator.java
@@ -0,0 +1,9 @@
+public class Calculator {
+   
+    // Group 1: Modulos (Members: Mirandilla,Gardoce,Gupid,Ongue,Gigante,Capuno,Intia,Fernandez)
+    public static int mod(int a, int b) {
+        return b % a;
+    }
+
+    // Other groups will add their methods here
+}
---
+++ Groupwork.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class GroupWork   {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+       System.out.println("\nCOLLABORATIVE CALCULATOR");
+        System.out.println("Enter first number: ");
+        int num1 = scanner.nextInt();
+        System.out.print("Enter second number: ");
+        int num2 = scanner.nextInt();
+       
+     
+        System.out.println("Remainder: " + Calculator.mod(num1, num2));  // Group 5's method
+        
+        scanner.close();
+    }
+}
