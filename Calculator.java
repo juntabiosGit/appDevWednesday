9363a8080a16677ca2eb93da25b1ddbd5a95a6c7
afernandezgit <your-email@example.com>
2025-03-19 07:04:33 +0800

    Group 5: Added Modulos Method(Mirandilla,Gardoce,Gupid,Ongue,Gigante,Capuno,Intia,Fernandez,Yamson,Fabro)

--- Calculator.java
+++ Calculator.java
@@ -1,21 +1,9 @@
-// Calculator.java
 public class Calculator {
-    // Group 3: multiply(Members: Elloren)
-    public static int multiple(int a, int b) {
-        return a * b;
-    }
    
-    // Group 2: Subtraction (Members: Brisulda)
-    public static int subtract(int a, int b) {
-        return a - b;
+    // Group 1: Modulos (Members: Mirandilla,Gardoce,Gupid,Ongue,Gigante,Capuno,Intia,Fernandez,Yamson,Fabro)
+    public static int mod(int a, int b) {
+        return b % a;
     }
 
     // Other groups will add their methods here
-
-
-// Group 6: Square (Members: Orolfo, Laygan)
-public static int square(int a) {
-    return a * a;
-}
-
 }
