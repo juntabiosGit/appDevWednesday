ab3d6bf71259520d3a47d1dbcddf02e33d479165
Joel Manuba <jpmanuba@paterostechnologicalcollege.edu.ph>
2025-03-19 18:20:14 +0800

    Group 4: Added divide method (Manuba, Pacencia, Bea, Mangunay, Lampa)

--- Calculator.java
+++ Calculator.java
@@ -9,8 +9,10 @@ public class Calculator {
     public static int subtract(int a, int b) {
         return a - b;
     }
-    // Other groups will add their methods here
-
+    // Group 4: Divide (Members: Manuba, Pacencia, Bea, Mangunay, Lampa)
+    public static double divide (int a, int b) {
+        return (double) a / b;
+    }
     // Group 6: Square (Members: Orolfo, Laygan)
     public static int square(int a) {
         return a * a;
