6f499955d19a96582869bbc1bf5e4e86d7c13dbc
juntabiosGit <165021843+juntabiosGit@users.noreply.github.com>
2025-03-19 16:27:11 +0800

    remove spaces

--- GroupWork.java
+++ GroupWork.java
@@ -3,24 +3,17 @@ import java.util.Scanner;
 public class GroupWork {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         System.out.println("\nCOLLABORATIVE CALCULATOR");
         System.out.print("Enter first number: ");
         int num1 = scanner.nextInt();
         System.out.print("Enter second number: ");
         int num2 = scanner.nextInt();
-
-        
         System.out.println("Quotient: " + Calculator.divide(num1, num2)); 
-
-        scanner.close();
-
-        System.out.println("\nCOLLABORATIVE CALCULATOR");
+         System.out.println("\nCOLLABORATIVE CALCULATOR");
         System.out.println("Enter first number: ");
         int num1 = scanner.nextInt();
         System.out.print("Enter second number: ");
         int num2 = scanner.nextInt();
-        
         System.out.println("Sum: " + Calculator.multiple(num1, num2)); // Group 3's method
         System.out.println("subtract: " + Calculator.subtract(num1, num2)); // Group 2's method
         System.out.println("Minimum: " + Calculator.min(num1, num2)); // Group 9's method
@@ -30,7 +23,6 @@ public class GroupWork {
         System.out.println("Division result: " + Calculator.divide(num1, num2));
         System.out.println("Minimum: " + Calculator.min(num1, num2)); // Group 9's method
         System.out.println("Are numbers equal? " + Calculator.isEqual(num1, num2));  // Group 10's method
-     
         scanner.close();
     }
 
