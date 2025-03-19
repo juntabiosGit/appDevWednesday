4656f86ccc928f3096fa09e7517ce26ac66e17ed
Joel Manuba <jpmanuba@paterostechnologicalcollege.edu.ph>
2025-03-19 15:17:32 +0800

    Group 4: Added divide method (Joel Manuba, Jayvon Pacencia, Awin Mangunay, Chelsie Kaye Bea, Noah Margate Lampa)

--- Calculator.java
+++ Calculator.java
@@ -10,6 +10,13 @@ public class Calculator {
         return a - b;
     }
 
+    // Group 4: Divide (Members: Jowel)
+    public static double divide(int a, int b) {
+         return (double) a / b; 
+    }
+
+
+
     // Other groups will add their methods here
 
 
--- GroupWork.java
+++ GroupWork.java
@@ -2,8 +2,10 @@ import java.util.Scanner;
 
 public class GroupWork   {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
-       System.out.println("\nCOLLABORATIVE CALCULATOR");
+
+        System.out.println("\nCOLLABORATIVE CALCULATOR");
         System.out.println("Enter first number: ");
         int num1 = scanner.nextInt();
         System.out.print("Enter second number: ");
@@ -14,6 +16,8 @@ public class GroupWork   {
         System.out.println("Sum: " + Calculator.multiple(num1, num2));  // Group 2's method
 
         System.out.println("subtract: " + Calculator.add(num1, num2));  // Group 2's method
+
+        System.out.println("Division result: " + Calculator.divide(num1, num2));
         
 
         //System.out.println("Are numbers equal? " + Calculator.isEqual(num1, num2));  // Group 10's method
