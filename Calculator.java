107c770f9cdcc5cfe91ed22ff59deb715ff25e21
Xander <xdolimberio@paterostechnologicalcollege.edu.ph>
2025-03-19 17:06:42 +0800

    Delete Unwanted File & Modified Files

--- Calculator.java
+++ Calculator.java
@@ -1,63 +1,14 @@
-
-// Group 4: divide(Members: Mangunay, Lampa, Pacencia, Bea, Manuba
-public static int divide(int a, int b) {
-    if (b == 0) {
-        System.out.println("Error: Division by zero is not allowed.");
-        return 0; // 
-    }
-    return a/ b;
-}
-
+// Calculator.java
 public class Calculator {
-
-    // Group 3: multiply(Members: Elloren, Fonte, Cabangunay, Beo, Reginio, Miranda)
-
-
-    // Group 3: multiply(Members: Elloren,Fonte,Cabangunay,Beo)
- 
-public class Calculator {
-   
-    // Group 5: Modulus (Members:Mirandilla,Gardoce,Gupid)
-    public static int mod(int a, int b) {
-        return a % b;
-    }
-
-}
-
-
-
-    // Group 5: Modulus (Members: Gardoce)
-
-    public static int mod(int a, int b) {
-        return a % b;
-    }
-
-
-    // Other groups will add their methods here
-}
-
     // Group 3: multiply(Members: Elloren)
-
-
     public static int multiple(int a, int b) {
         return a * b;
     }
 
-    
-    // Group 2: Subtraction (Members: Brisulda, Nati )
-
-
+    // Group 2: Subtraction (Members: Brisulda)
     public static int subtract(int a, int b) {
         return a - b;
     }
-
-    // Group 4: Divide (Members: Manuba, Bea, Paciencia, Lampa, Mangunay)
-    public static double divide(int a, int b) {
-         return (double) a / b; 
-    }
-
-
-
     // Other groups will add their methods here
 
     // Group 6: Square (Members: Orolfo, Laygan)
@@ -65,26 +16,9 @@ public class Calculator {
         return a * a;
     }
 
-
     // Group 9: Min (Members: Olimberio, Sugui, Impreso, Sapico, Marcos)
     public static int min(int a, int b) {
         return Math.min(a, b);
     }
 
-// Group 6: Square (Members: Orolfo, Laygan)
-public static int square(int a) {
-    return a * a;
-
 }
-
- 
-// Group 9: Min (Members: Olimberio, Sugui, Impreso, Sapico, Marcos)
-public static int min(int a, int b) {
-    return Math.min(a, b);
-}
-
-
-// Group 10:isEqual  ( Stephen)
-public static boolean isEqual(int a, int b) {
-    return a == b;
-
